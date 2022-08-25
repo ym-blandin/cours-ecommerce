81d421465cecbee7b7641b1222799f77374f5cc1
Yves-Marie Blandin <yves-marie.blandin@humansconnexion.com>
2022-08-25 10:48:30 +0200

    initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    public function index()
+    {
+        dd("Cool ça marche !");
+    }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, schemes={"http", "https"})
+     */
+    public function test(Request $request, $age)
+    {
+        // $age = $request->attributes->get('age');
+
+        return new Response("Vous avez $age ans !");
+    }
+}
